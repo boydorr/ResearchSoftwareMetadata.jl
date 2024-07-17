ad9b080f419b7056353a8317f8e272c4c87f2bfc
Documenter.jl <documenter@juliadocs.github.io>
2024-07-17 16:48:48 +0000

    build based on 970ec9f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ResearchSoftwareMetadata","category":"page"},{"location":"#ResearchSoftwareMetadata","page":"Home","title":"ResearchSoftwareMetadata","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ResearchSoftwareMetadata.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ResearchSoftwareMetadata]","category":"page"}]
+}
